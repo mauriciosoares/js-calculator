6a15e72ec8b2014d0c534ea30d1edcb6a5ca2073
Mauricio Soares <m.soares2603@gmail.com>
2015-08-04 22:16:27 -0300

    update screen tests

--- tests/screenSpec.js
+++ tests/screenSpec.js
@@ -1,14 +1,16 @@
 describe('Testing Screen', function() {
+  var screen;
+  beforeEach(function() {
+    screen = new Screen();
+  });
+
   it('Should return a new instance of Screen', function() {
-    var screen = new Screen();
     expect(screen instanceof Screen).toBeTruthy();
   });
   it('Should create a jquery instance inside the $el index of the instance', function() {
-    var screen = new Screen();
     expect(screen.$el instanceof jQuery).toBeTruthy();
   });
   it('Should render the given number inside the calculator whe using the render method', function() {
-    var screen = new Screen();
     screen.render('1234');
     expect(screen.$el.val()).toEqual('1234');
   });
