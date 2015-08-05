1121a64db227078236325edf75ec660f973d6169
Mauricio Soares <m.soares2603@gmail.com>
2015-08-04 21:47:49 -0300

    Tests on screen done

--- tests/screenSpec.js
+++ tests/screenSpec.js
@@ -1,5 +1,15 @@
 describe('Testing Screen', function() {
-  it('Should return a new instance of Screen');
-  it('Should create an element inside the $el index of the instance');
-  it('Should render the given number inside the calculator whe using the render method');
+  it('Should return a new instance of Screen', function() {
+    var screen = new Screen();
+    expect(screen instanceof Screen).toBeTruthy();
+  });
+  it('Should create a jquery instance inside the $el index of the instance', function() {
+    var screen = new Screen();
+    expect(screen.$el instanceof jQuery).toBeTruthy();
+  });
+  it('Should render the given number inside the calculator whe using the render method', function() {
+    var screen = new Screen();
+    screen.render('1234');
+    expect(screen.$el.val()).toEqual('1234');
+  });
 });
